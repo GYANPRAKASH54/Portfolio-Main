c3aca7cf0cea5a472f39785695751005ad6c124d
GYAN PRAKASH <gyanp2552@gmail.com>
2025-05-02 20:17:39 +0530

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,5 @@
 import { Project } from '../types';
-import noteImage from '../assests/download (2).jpg'
-
-// Import local images
-import notesAppImage from '../assets/download(2).jpg';
+import noteImage from '../assests/download(2).jpg'
 import pnrImage from '../assets/pnr.png';
 
 export const projects: Project[] = [
@@ -11,7 +8,7 @@ export const projects: Project[] = [
     title: 'Notes Management App',
     description: 'A productivity app that helps users manage notes.',
     technologies: ['React', 'Node.js', 'Express.js', 'MongoDb'],
-    image: "/src/assests/download (2).jpg",
+    image: "noteImage",
     github:'https://github.com/GYANPRAKASH54/Notes-Me.-Web-App',
     link: '/',
   },
